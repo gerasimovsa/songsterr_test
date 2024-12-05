d7b15c4fd2f43f31c29e3a1b843e9b38c8646369
ra9_adept <gerasimov.ra9@gmail.com>
2024-12-06 00:36:12 +0300

    Renamed RegistrationPage to SignInPage. Added tests for New Tab page and My Tabs pages.

--- src/test/java/tests/MyTabsPageTests.java
+++ src/test/java/tests/MyTabsPageTests.java
@@ -1,13 +1,15 @@
 package tests;
 
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Condition.empty;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class MyTabsPageTests {
@@ -60,7 +62,9 @@ public class MyTabsPageTests {
     }
 
     @Test
-    void ipload() {
+    void removeSongFromContributions() {
+
+        Configuration.timeout = 12000;
 
         open("https://songsterr.com");
 
@@ -70,22 +74,29 @@ public class MyTabsPageTests {
         $("#signin").click();
         $("#menu-account").lastChild().shouldHave(text("regular_s"));
 
-        $("#menu-search").click();
-        $("#panel-search input").shouldBe(empty).setValue("happy ending the strokes");
-        $$("[data-field='name']").findBy(exactText("Happy Ending")).click();
 
-        $("#favorite-toggle").shouldBe(visible).click();
+        $("#menu-submit").click();
+        $("[name='title']").shouldBe(empty).setValue("My Test Song");
+        $("[name='artist']").shouldBe(empty).setValue("Test Band");
+        $("[aria-label='Create blank']").click();
+        $("#panel-submit").shouldNotBe(visible);
+
         $("#menu-favorites").click();
-        $("#song-options-button").click();
-        $("[data-feature='remove']").click();
+        $("#title-contributions1").ancestor("a").click();
+        $$("[data-field='name']").findBy(text("My Test Song")).click();
+        $("#song-delete-icon").shouldBe(visible).click();
+        switchTo().alert().sendKeys("test band, My Test Song");
+        confirm();
 
-        $("[data-stub='nofavorites']").shouldBe(visible);
+
+        $("#apptab span").shouldHave(text("The tab was successfully deleted!"));
+
+
+
 
     }
 
 }
 
 
-
-
-//todo header - create tab, upload gp, delete song
+//todo new tab -  create playlist
--- src/test/java/tests/NewTabsPageTests.java
+++ src/test/java/tests/NewTabsPageTests.java
@@ -1,6 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
 
 public class NewTabsPageTests {
@@ -8,8 +16,80 @@ public class NewTabsPageTests {
     @Test
     void createNewBlankSong() {
 
-        //todo new tab - new blank, upload gp, delete song, create playlist
+        Configuration.timeout = 10000;
+
+        open("https://songsterr.com");
+
+        $("#menu-signin").click();
+        $("[name='email']").shouldBe(empty).setValue("gerasimovsa20@gmail.com");
+        $("[name='password']").shouldBe(empty).setValue("newpassword1");
+        $("#signin").click();
+        $("#menu-account").lastChild().shouldHave(text("regular_s"));
+
+        $("#menu-submit").click();
+        $("[name='title']").shouldBe(empty).setValue("My Test Song");
+        $("[name='artist']").shouldBe(empty).setValue("Test Band");
+        $("[aria-label='Create blank']").click();
+
+
+        $("#panel-submit").shouldNotBe(visible);
+        $("[aria-label='title']").shouldHave(text("My Test Song"));
+        $("[aria-label='artist']").shouldHave(text("Test Band"));
+
+    }
+
+    @Test
+    void createNewSongFromGPFile() {
+
+        Configuration.timeout = 10000;
+
+        open("https://songsterr.com");
+
+        $("#menu-signin").click();
+        $("[name='email']").shouldBe(empty).setValue("gerasimovsa20@gmail.com");
+        $("[name='password']").shouldBe(empty).setValue("newpassword1");
+        $("#signin").click();
+        $("#menu-account").lastChild().shouldHave(text("regular_s"));
+
+        $("#menu-submit").click();
+        $("[aria-label='Upload GuitarPro']").click();
+        $("[name='title']").shouldBe(empty).setValue("Song from GP File");
+        $("[name='artist']").shouldBe(empty).setValue("Test GP Band");
+
+
+        File gpFile = new File("src/test/resources/my_gp_file.gp3");
+        $("#tabFile").uploadFile(gpFile);
+        $("[aria-label='Upload']").click();
+
+
+        $("#panel-submit").shouldNotBe(visible);
+        $("[aria-label='title']").shouldHave(text("Song from GP File"));
+        $("[aria-label='artist']").shouldHave(text("Test GP Band"));
+
+    }
+
+    @Test
+    void cannotCreateSongWithBlankTitle() {
+
+        Configuration.timeout = 10000;
+
+        open("https://songsterr.com");
+
+        $("#menu-signin").click();
+        $("[name='email']").shouldBe(empty).setValue("gerasimovsa20@gmail.com");
+        $("[name='password']").shouldBe(empty).setValue("newpassword1");
+        $("#signin").click();
+        $("#menu-account").lastChild().shouldHave(text("regular_s"));
+
+        $("#menu-submit").click();
+        $("[name='artist']").shouldBe(empty).setValue("Metallica");
+        $("[aria-label='Create blank']").click();
+
+
+        $("[name='title']").
+                sibling(1).shouldHave(text("Should not be empty"));
 
     }
 
 }
+
